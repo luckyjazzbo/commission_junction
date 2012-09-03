96bcfdddbb81da4d480eb7a584615dd3c9176c03
Albert Vernon <aev@vernon.nu>
2012-09-03 17:25:28 -0400

    Use "simplecov" gem for code coverage during testing.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source :rubygems
 
 # Specify your gem's dependencies in commission_junction.gemspec
 gemspec
+
+gem 'simplecov', :require => false, :group => :test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
+require 'simplecov'
 require 'test/unit'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+SimpleCov.start
 require 'commission_junction'
